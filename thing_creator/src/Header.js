34271ff2036642660eb5ced156bc5126224b1529
natalie sinner <nataliesinner@hotmail.com>
2020-10-03 14:16:44 -0700

    working on form

---
+++ thing_creator/src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Header extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <>
+            <h2>This is our Number of Things</h2>
+            </>
+        )
+    }
+}
+
+
+export default Header;
